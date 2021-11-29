074f9bde70ce6bdced76c4c30548fb11821ec357
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-11-30 01:41:59 +0900

    Feat: OAuth기능 추가
    구글 소셜 로그인 기능 추가

---
+++ src/main/java/com/test/admin/web/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.test.admin.web.config.auth;
+
+import com.test.admin.web.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable() //(2)
+                .and()
+                .authorizeRequests() //(3)
+                .antMatchers("/", "css/**", "/images/**", "/js/**", "h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //(4)
+                .anyRequest().authenticated() //(5)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/") //(6)
+                .and()
+                .oauth2Login() //(7)
+                .userInfoEndpoint() //(8)
+                .userService(customOAuth2UserService); //(9)
+    }
+}
